9306da0221702429e44f2e396c49bff77faa2e6c
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2023-01-07 19:07:16 +0800

    Feature: Added AppLayout

--- src/Layout/AppLayout/AppLayout.component.tsx
+++ src/Layout/AppLayout/AppLayout.component.tsx
@@ -1,11 +1,17 @@
 import {ReactChildren} from "../../types"
-
+import Box from "@mui/material/Box";
+import CssBaseline from "@mui/material/CssBaseline"
 
 export const AppLayout:React.FC<ReactChildren> = ({children}) => {
   return (
-    <div>
-      This is App Layout
-      {children}
-    </div>
+    <Box sx={{display:"flex"}}>
+      <CssBaseline />
+      <meta name="viewport" content="initial-scale=1, width=device-width" />
+      <Box component={"main"} sx={{display:"flex"}}>
+        This is App Layout
+        {children}
+      </Box>
+    </Box>
   )
 }
+

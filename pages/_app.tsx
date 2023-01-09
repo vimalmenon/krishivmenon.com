cc191364bc079903f5002efab801dba03f005ce2
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2023-01-10 05:05:02 +0800

    Feature:Added Theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,17 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import {AppLayout} from "../src/Layout/AppLayout"
+import {ThemeProvider} from "@mui/material/styles";
+import {theme} from "../src/Config/theme"
+
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <AppLayout>
+      <ThemeProvider theme={theme}>
       <Component {...pageProps} />
+      </ThemeProvider>
     </AppLayout>
   )
 }
+

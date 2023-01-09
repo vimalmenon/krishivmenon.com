1489b9d18d9f3b7ce06781a760ba5261cf1b5cf0
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2023-01-09 19:03:35 +0800

    Chore: removed unwanted code

--- src/Layout/AppLayout/Footer/Footer.component.tsx
+++ src/Layout/AppLayout/Footer/Footer.component.tsx
@@ -4,7 +4,6 @@ import {FooterStyle} from "./Footer.style";
 export const Footer:React.FC = () => {
   return (
     <Container component={"footer"} sx={{flex: "0 0 20px"}}>
-      This is footer
       <FooterStyle>
       copywrite @ 2023, all rights reserved
     </FooterStyle>

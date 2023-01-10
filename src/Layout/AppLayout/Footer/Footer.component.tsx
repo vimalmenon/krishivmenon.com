37695ed293c5dc0a9fb7b38498f2034400c24505
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2023-01-11 05:42:25 +0800

    Feature: Add more path

--- src/Layout/AppLayout/Footer/Footer.component.tsx
+++ src/Layout/AppLayout/Footer/Footer.component.tsx
@@ -5,9 +5,9 @@ import { FooterStyle } from './Footer.style';
 
 export const Footer: React.FC = () => {
   return (
-    <Container component={'footer'} sx={{ flex: '0 0 20px' }}>
+    <Container component={'footer'} sx={{ flex: '0 0 40px' }}>
       <MuiContainer maxWidth="lg">
-        <FooterStyle>copywrite @ 2023, all rights reserved</FooterStyle>
+        <FooterStyle>&#169; 2023, All rights reserved</FooterStyle>
       </MuiContainer>
     </Container>
   );
---
+++ src/Page/Home/Home.component.tsx
@@ -0,0 +1,9 @@
+import { Container } from '@style';
+
+export const Main: React.FC = () => {
+  return (
+    <Container component={'div'}>
+      <div>This is Home Page</div>
+    </Container>
+  );
+};

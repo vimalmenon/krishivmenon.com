7365b82fdb643497796ce6bdbef2233f94ea60d9
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2023-01-11 05:53:23 +0800

    Feature:Add common

--- src/Page/Home/Home.component.tsx
+++ src/Page/Home/Home.component.tsx
@@ -1,8 +1,10 @@
+import { MetaData } from '@common';
 import { Container } from '@style';
 
 export const Main: React.FC = () => {
   return (
     <Container component={'div'}>
+      <MetaData />
       <div>This is Home Page</div>
     </Container>
   );

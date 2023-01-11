0fd8c024a699446b04c897f0eddce98444506c62
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2023-01-11 09:03:27 +0800

    Feature:Added Context value

--- src/Common/AppContext/AppContext.service.ts
+++ src/Common/AppContext/AppContext.service.ts
@@ -1,3 +1,4 @@
 import React from 'react';
+import {IAppContext} from "./AppContext";
 
-export const Context = React.createContext({});
+export const Context = React.createContext<IAppContext>({theme: "dark"});
--- src/Common/AppContext/Context.d.ts
+++
@@ -1,3 +0,0 @@
-export interface IContext {
-  theme: 'dark' | 'light';
-}

d7f79b391366a4e17b9595221cd3b2c9ae0b06fb
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2023-01-11 13:42:34 +0800

    Feature: Added AppLayout to Page

--- src/Layout/AppLayout/AppLayout.component.tsx
+++ src/Layout/AppLayout/AppLayout.component.tsx
@@ -1,18 +1,20 @@
 import Box from '@mui/material/Box';
 import { ReactChildren } from '@types';
 
+import { BaseLayout } from '../';
 import { Footer } from './Footer';
 import { Header } from './Header';
-
 export const AppLayout: React.FC<ReactChildren> = ({ children }) => {
   return (
-    <Box sx={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
-      <meta name="viewport" content="initial-scale=1, width=device-width" />
-      <Header />
-      <Box component={'main'} sx={{ display: 'flex', flex: '1 1 auto' }}>
-        {children}
+    <BaseLayout title="Home Page">
+      <Box sx={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+        <Header />
+        <Box component={'main'} sx={{ display: 'flex', flex: '1 1 auto' }}>
+          {children}
+        </Box>
+        <Footer />
       </Box>
-      <Footer />
-    </Box>
+    </BaseLayout>
   );
 };

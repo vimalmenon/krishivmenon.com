80f64294306caa27e441d5b84589af1d45fd177e
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2023-01-12 12:18:21 +0800

    Feature: Added Theme Swticher

--- src/Layout/AppLayout/Header/Header.component.tsx
+++ src/Layout/AppLayout/Header/Header.component.tsx
@@ -1,9 +1,12 @@
+import { context } from '@common';
 import Switch from '@mui/material/Switch';
 import { Container } from '@style';
 
 import { HeaderSwitch } from './Header.style';
 
 export const Header: React.FC = () => {
+  const { theme } = context.useContext();
+  const { switchTheme } = context.useThemeSwitcher();
   return (
     <Container component="header" sx={{ display: 'flex', flex: '0 0 50px' }}>
       <Container component={'div'} sx={{ flex: '1 1 auto' }}>
@@ -11,7 +14,7 @@ export const Header: React.FC = () => {
       </Container>
       <Container component={'div'} sx={{ flex: '0 0 auto' }}>
         <HeaderSwitch>
-          <Switch />
+          <Switch checked={theme === 'dark'} onChange={switchTheme} />
         </HeaderSwitch>
         <HeaderSwitch>
           <Switch />

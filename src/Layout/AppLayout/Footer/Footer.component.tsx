9fe54410b0b9b55824c9d0c4f1a34431064d27f6
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2023-01-14 06:20:42 +0800

    Chore: Fix the Footer

--- src/Layout/AppLayout/Footer/Footer.component.tsx
+++ src/Layout/AppLayout/Footer/Footer.component.tsx
@@ -1,12 +1,13 @@
+import { Footer as FooterCommon } from '@common';
 import { Container } from '@style';
 
-import { FooterStyle, FooterContainer } from './Footer.style';
+import { FooterContainer } from './Footer.style';
 
 export const Footer: React.FC = () => {
   return (
     <Container component={'footer'} sx={{ flex: '0 0 40px' }}>
       <FooterContainer maxWidth="lg">
-        <FooterStyle>&#169; 2023, All rights reserved</FooterStyle>
+        <FooterCommon />
       </FooterContainer>
     </Container>
   );

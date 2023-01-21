8fbe02733c24aa14d6ec370c6b99ed5b8ea99245
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2023-01-21 13:05:45 +0800

    Create a delete for Item

--- src/Common/FileUpload/Item/Item.hook.ts
+++ src/Common/FileUpload/Item/Item.hook.ts
@@ -8,14 +8,14 @@ import { apis } from '@utility';
 import { IUseItem } from './Item';
 
 export const useItem: IUseItem = (file, uid, fileType) => {
-  const [loading] = React.useState<boolean>(true);
+  const [loading, setLoading] = React.useState<boolean>(true);
   const [isDeleted, setDeleted] = React.useState<boolean>(false);
   const refs = React.useRef<boolean>(true);
   const { makeApiCall } = useCommonApiContext();
   const extension = FileTypeMapping[file.type];
   React.useEffect(() => {
     if (!refs.current) {
-      makeApiCall(
+      makeApiCall<File, unknown>(
         apis.uploadToS3({
           folder: StorageFolderMapping[fileType],
           fileName: uid,
@@ -27,7 +27,17 @@ export const useItem: IUseItem = (file, uid, fileType) => {
       // refs.current = false;
     }
   }, []);
-  const onDelete: IGenericMethod = () => {
+  const onDelete: IGenericMethod = async () => {
+    setLoading(false);
+    await makeApiCall<unknown, unknown>(
+      apis.deleteFromS3({
+        folder: StorageFolderMapping[fileType],
+        fileName: uid,
+        extension,
+        fileType: file.type,
+      })
+    );
+    setLoading(false);
     setDeleted(true);
   };
   return {

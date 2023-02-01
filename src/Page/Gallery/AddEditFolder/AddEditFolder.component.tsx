019b18f66beb179b76840a5d4cf3de7f64080627
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2023-02-02 06:45:31 +0800

    Removed unwanted package

--- src/Page/Gallery/AddEditFolder/AddEditFolder.component.tsx
+++ src/Page/Gallery/AddEditFolder/AddEditFolder.component.tsx
@@ -1,3 +1,4 @@
+import { FileUpload } from '@common';
 import { CommonIcons } from '@constant';
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
@@ -32,7 +33,7 @@ export const AddEditFolder: React.FC = () => {
         />
       </Container>
       <Container component={'div'} sx={{ flex: '1' }}>
-        &nbsp;
+        <FileUpload fileType={'image'} />
       </Container>
       <Container component={'div'} sx={{ justifyContent: 'space-between' }}>
         <Button variant="contained" onClick={onAddFolderSave}>

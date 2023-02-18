541209569c92f769ba5eaf6265350993342d7a13
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2023-02-19 07:30:07 +0800

    Updated PageLayout

--- src/Layout/PageLayoutFooter/PageLayoutFooter.component.tsx
+++ src/Layout/PageLayoutFooter/PageLayoutFooter.component.tsx
@@ -1,10 +1,10 @@
 import { FooterStyle, PageLayoutFooterRoot } from './PageLayoutFooter.style';
-import packageJson from "../../../package.json";
+import packageJson from '../../../package.json';
 
 export const PageLayoutFooter: React.FC = () => {
   return (
     <PageLayoutFooterRoot sx={{ borderColor: 'divider' }}>
-      <FooterStyle>&#169; 2023, All rights reserved v{packageJson.version}</FooterStyle>
+      <FooterStyle>&#169; 2023, All rights reserved (v{packageJson.version})</FooterStyle>
     </PageLayoutFooterRoot>
   );
 };

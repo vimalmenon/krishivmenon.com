e2c04dec6eaca5594b157f273d210190854bd724
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2023-02-18 11:32:45 +0800

    Added Profile to side Nav

--- src/Common/SideNavigation/SideNavigation.component.tsx
+++ src/Common/SideNavigation/SideNavigation.component.tsx
@@ -1,16 +1,33 @@
+import { useCommonAuthProvider } from '@context';
 import { navigation } from '@data';
+import Avatar from '@mui/material/Avatar';
+import Divider from '@mui/material/Divider';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
 import { useRouter } from 'next/router';
 
-import { SideNavigationList, SideNavigationRoot } from './SideNavigation.style';
+import {
+  SideNavigationList,
+  SideNavigationProfile,
+  SideNavigationRoot,
+} from './SideNavigation.style';
 
 export const SideNavigation: React.FC = () => {
   const { push, asPath } = useRouter();
+  const { user } = useCommonAuthProvider();
   const { NavigationList } = navigation;
   return (
     <SideNavigationRoot>
+      {user && (
+        <SideNavigationProfile>
+          <div className="profile">
+            <Avatar alt={user.name} src={user.profile} />
+          </div>
+          <div className="profile-detail">{user.name}</div>
+        </SideNavigationProfile>
+      )}
+      <Divider></Divider>
       <SideNavigationList dense>
         {NavigationList.map((navigation) => {
           if (navigation.show) {

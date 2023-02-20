c68b5f8bee7cd9b24e18bf496f3bdfccae62bf95
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2023-02-20 19:21:05 +0800

    Removed unwanted method

--- src/Utility/index.ts
+++ src/Utility/index.ts
@@ -1,13 +1,7 @@
-import { AnyType, IGeneric } from '@types';
+import { AnyType } from '@types';
 
 export const NotImplemented = (): AnyType => {
   throw new Error('Function not implemented');
-};
-
-export const checkTruthy: IGeneric<string | undefined | null, boolean> = (value) => {
-  if (!value) return false;
-  if (value === null) return false;
-  return true;
 };
 
 export { apis, getBaseUrl } from './apis';

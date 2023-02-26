9c0e637b5b9c492964809594c28c52a6102d4a4f
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2023-02-26 19:04:37 +0800

    Working on Login

--- src/Page/Login/Login.component.tsx
+++ src/Page/Login/Login.component.tsx
@@ -1,23 +1,32 @@
+import React from 'react';
+
 import { Providers } from '@constant';
-import Button from '@mui/material/Button';
+import LoadingButton from '@mui/lab/LoadingButton';
+import { IGenericParam } from '@types';
 
 import { onClick } from './Login.service';
 import { LoginRoot } from './Login.style';
 
 export const Login: React.FC = () => {
+  const [loading, setLoading] = React.useState<boolean>(false);
+  const onLogin: IGenericParam<string> = (provider) => {
+    setLoading(true);
+    onClick(provider);
+  };
   return (
     <LoginRoot>
       {Providers.map((provider, key) => {
         return (
-          <Button
-            variant="outlined"
+          <LoadingButton
             key={key}
+            onClick={() => onLogin(provider.provider)}
             startIcon={<provider.Icon />}
-            onClick={() => onClick(provider.provider)}
+            loading={loading}
+            loadingPosition="start"
+            variant="outlined"
           >
-            {' '}
-            Sign in with {provider.name}
-          </Button>
+            <span>Sign in with {provider.name}</span>
+          </LoadingButton>
         );
       })}
     </LoginRoot>

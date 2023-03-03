907f852febe3960dc372d7a75ec604db80b1e80b
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2023-03-03 13:35:25 +0800

    Fixing the build

--- src/Layout/EnvCheck/EnvCheck.component.tsx
+++ src/Layout/EnvCheck/EnvCheck.component.tsx
@@ -5,8 +5,6 @@ import { ReactChildren } from '@types';
 
 export const EnvCheck: React.FC<ReactChildren> = ({ children }) => {
   React.useEffect(() => {
-    // eslint-disable-next-line no-console
-    console.log(ENV);
     for (const value of Object.values(ENV)) {
       if (!value) {
         throw new Error('Env values missing');

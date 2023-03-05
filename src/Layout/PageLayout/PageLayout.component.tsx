7082c4085831a5f83553cc1f590a62add430b87d
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2023-03-05 17:27:23 +0800

    More refactoring of code

--- src/Layout/PageLayout/PageLayout.component.tsx
+++ src/Layout/PageLayout/PageLayout.component.tsx
@@ -19,52 +19,30 @@ import {
 } from '../';
 
 export const PageLayout: React.FC<ReactChildren & IBaseLayout> = ({ children, title }) => {
-  useUser();
   const { authStatus } = useCommonContext();
   if (authStatus === AuthStatus.NotAuthenticated) {
     return (
-      <OtherLayout>
-        <CssBaseline />
-        <MetaData title={navigation.Login.title} />
-        <PageLayoutHeader />
-        <MainPageContent>
-          <Login />
-        </MainPageContent>
-        <PageLayoutFooter />
-      </OtherLayout>
+      <OtherPage title={navigation.Login.title}>
+        <Login />
+      </OtherPage>
     );
   }
   if (authStatus === AuthStatus.UnAuthorized) {
     return (
-      <OtherLayout>
-        <CssBaseline />
-        <MetaData title={navigation.Unauthenticated.title} />
-        <PageLayoutHeader />
-        <MainPageContent>
-          <div>You are not authorized to this website.</div>
-        </MainPageContent>
-        <PageLayoutFooter />
-      </OtherLayout>
+      <UnauthorizedPage title={navigation.Unauthenticated.title}>
+        <div>You are not authorized to this website.</div>
+      </UnauthorizedPage>
     );
   }
   if (authStatus === AuthStatus.Authenticating) {
     return (
-      <OtherLayout>
-        <CssBaseline />
-        <MetaData title={title} />
-        <PageLayoutHeader />
-        <MainPageContent>
-          <div>Authenticating...</div>
-        </MainPageContent>
-        <PageLayoutFooter />
-      </OtherLayout>
+      <UnauthorizedPage title={title}>
+        <div>Authenticating...</div>
+      </UnauthorizedPage>
     );
   }
   return (
-    <MainPageLayout>
-      <CssBaseline />
-      <MetaData title={title} />
-      <PageLayoutHeader />
+    <UserPage title={title}>
       <ErrorBoundary
         sx={{
           gridArea: 'content',
@@ -80,6 +58,43 @@ export const PageLayout: React.FC<ReactChildren & IBaseLayout> = ({ children, ti
           </MainPageContent>
         </EnvCheck>
       </ErrorBoundary>
+    </UserPage>
+  );
+};
+
+export const OtherPage: React.FC<ReactChildren & IBaseLayout> = ({ title, children }) => {
+  return (
+    <OtherLayout>
+      <CssBaseline />
+      <MetaData title={title} />
+      <PageLayoutHeader />
+      <MainPageContent>{children}</MainPageContent>
+      <PageLayoutFooter />
+    </OtherLayout>
+  );
+};
+
+export const UnauthorizedPage: React.FC<ReactChildren & IBaseLayout> = ({ title, children }) => {
+  useUser();
+  return (
+    <OtherLayout>
+      <CssBaseline />
+      <MetaData title={title} />
+      <PageLayoutHeader />
+      <MainPageContent>{children}</MainPageContent>
+      <PageLayoutFooter />
+    </OtherLayout>
+  );
+};
+
+export const UserPage: React.FC<ReactChildren & IBaseLayout> = ({ title, children }) => {
+  useUser();
+  return (
+    <MainPageLayout>
+      <CssBaseline />
+      <MetaData title={title} />
+      <PageLayoutHeader />
+      {children}
       <PageLayoutFooter />
     </MainPageLayout>
   );

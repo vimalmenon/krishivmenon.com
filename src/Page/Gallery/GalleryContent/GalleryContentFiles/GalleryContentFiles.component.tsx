91c4f0290fda00aa9716485704ba137157aab6a0
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2023-06-30 19:31:35 +0800

    Working on usePagination

--- src/Page/Gallery/GalleryContent/GalleryContentFiles/GalleryContentFiles.component.tsx
+++ src/Page/Gallery/GalleryContent/GalleryContentFiles/GalleryContentFiles.component.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import Pagination from '@mui/material/Pagination';
 
 import { FileAction } from '@common';
+import { usePagination } from '@hook';
 
 import { useFileHelper, useFolderHelper } from '../../Gallery.service';
 import { GalleryContentFilesStyle } from '../GalleryContent.style';
@@ -10,22 +11,17 @@ import { GalleryContentFilesStyle } from '../GalleryContent.style';
 export const GalleryContentFiles: React.FC = () => {
   const { currentFolder } = useFolderHelper();
   const { onFileMoveRequest, onFileDeleteRequest, onViewFile, onFileEditSave } = useFileHelper();
-  const [page, setPage] = React.useState<number>(0);
-  const handleChange = (event: React.ChangeEvent<unknown>, value: number) => {
-    setPage(value - 1);
-  };
+  const { page, handleChange, paginationCount, pageStart, pageEnd } = usePagination({
+    fileLength: currentFolder.files.length,
+    pageSize: 10,
+  });
   return (
     <>
       {currentFolder.files.length ? (
         <>
-          <Pagination
-            count={Math.ceil(currentFolder.files.length / 10)}
-            page={page + 1}
-            onChange={handleChange}
-            size="large"
-          />
+          <Pagination count={paginationCount} page={page} onChange={handleChange} size="large" />
           <GalleryContentFilesStyle>
-            {[...currentFolder.files.slice(page * 10, page * 10 + 10)].map((file) => {
+            {[...currentFolder.files.slice(pageStart, pageEnd)].map((file) => {
               return (
                 <div
                   key={file.id}

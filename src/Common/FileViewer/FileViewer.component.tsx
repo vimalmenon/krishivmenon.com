25d323a6298334787385b8f701ad683de2344f8f
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2023-07-02 12:35:56 +0800

    Improve FileViewer implementation

--- src/Common/FileViewer/FileViewer.component.tsx
+++ src/Common/FileViewer/FileViewer.component.tsx
@@ -1,17 +1,18 @@
 import { Image, Video } from '@common';
+import { FileMapping } from '@constant';
 import { getS3BucketFullPath } from '@utility';
 
 import { IFileViewer } from './FileViewer';
 
 export const FileViewer: React.FC<IFileViewer> = ({ file, width, height }) => {
-  if (file.type === 'image/jpeg') {
+  FileMapping;
+  if (FileMapping[file.type] === 'image') {
     return (
       <Image src={getS3BucketFullPath(file.path)} height={height} width={width} alt={file.label} />
     );
   }
-  if (file.type === 'video/mp4' || file.type === 'video/quicktime') {
+  if (FileMapping[file.type] === 'video') {
     return <Video src={getS3BucketFullPath(file.path)} />;
   }
-  console.log(file);
   return null;
 };
--- src/Config/constants.ts
+++ src/Config/constants.ts
@@ -27,6 +27,11 @@ export const AcceptVideo = {
   'video/*': ['.mp4', '.mov'],
 };
 
+export const FileMapping: Record<string, string> = {
+  'image/jpeg': 'image',
+  'video/mp4': 'video',
+  'video/quicktime': 'video',
+};
 export const StorageKey = 'KM';
 
 export const AuthRefreshTime = 6300000;
